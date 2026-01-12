dab5d2978d2ac747d66cda679096b9cafc0ac2fe
kawn2103 <ksgking1226@gmail.com>
2026-01-12 16:46:37 +0900

    툰플릭스 첫 커밋

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:toonflixapp/screens/home_screen.dart';
+import 'package:toonflixapp/services/api_service.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(), 
+    );
+  }
+
+}
---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+          ),
+        )
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 50,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250, 
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 7,
+                        offset: const Offset(5, 5),
+                        color: Colors.black.withValues(alpha: 0.5),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    thumb,
+                    headers: const {
+                      'Referer': 'https://comic.naver.com',
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
